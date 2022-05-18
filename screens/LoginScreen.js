fa546c40d3d5ddf8bacdb7a154ecaaef3dfb81a7
Schadrack <chadrackngirimana@gmail.com>
2022-05-18 20:50:42 +0200

    loggin user

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../util/auth';
 
-function LoginScreen() {
-  return <AuthContent isLogin />;
+function LoginScreen({isLogin}) {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert('Authentication failed', 'Could not log you in. Please check your credential or try again later!');
+    }
+    
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging in..." />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler}/>;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,14 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_KEY = "AIzaSyAkydi3lvHoL5OyiqV0nO_tSSx6QvTP5WA";
 
-export async function createUser(email, password) {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }
-  );
+ const authenticate=async(mode, email, password)=> {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
 }
+
+export  const createUser=async(email, password)=> {
+  await authenticate("signUp", email, password);
+}
+
+export  const login = async (email, password) => {
+  await authenticate("signInWithPassword", email, password);
+};

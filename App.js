6f301cdfe156b25fafca05acd9744e6255e9d6ac
Schadrack <chadrackngirimana@gmail.com>
2022-05-19 10:50:23 +0200

    storing token on device

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import { useContext, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import AppLoading from "expo-app-loading";
+
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import { Colors } from "./constants/styles";
 import AuthContextProvider, { AuthContext } from "./store/auth-context";
-import { useContext } from "react";
 import IconButton from "./components/ui/IconButton";
 
 const Stack = createNativeStackNavigator();
@@ -56,6 +59,7 @@ function AuthenticatedStack() {
 
 function Navigation() {
   const authCtx = useContext(AuthContext);
+
   return (
     <NavigationContainer>
       {!authCtx.isAuthenticated && <AuthStack />}
@@ -64,12 +68,38 @@ function Navigation() {
   );
 }
 
+function Root() {
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem("token");
+
+      if (storedToken) {
+        authCtx.authenticate(storedToken);
+      }
+
+      setIsTryingLogin(false);
+    }
+
+    fetchToken();
+  }, []);
+
+  if (isTryingLogin) {
+    return <AppLoading />;
+  }
+
+  return <Navigation />;
+}
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );
--- store/auth-context.js
+++ store/auth-context.js
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useState, } from "react";
+
 
 export const AuthContext = createContext({
   token: "",
@@ -8,11 +10,15 @@ export const AuthContext = createContext({
 });
 const AuthContextProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState();
+
+  
   const authenticate = (token) => {
     setAuthToken(token);
+    AsyncStorage.setItem('token',token)
   };
   const logout = () => {
     setAuthToken("");
+    AsyncStorage.removeItem('token');
   };
   const value = {
     token: authToken,
